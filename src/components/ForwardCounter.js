1dcb9a9c668a5336635660b58ab5d26f2db78d3d
smdsaran <saranthuppaki056@gmail.com>
2021-07-19 16:23:07 +0530

    basic custom hook

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,9 @@
+import useCounter from "../custom-hook/custom-counter";
+import Card from "./Card";
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
