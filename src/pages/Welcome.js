eef611e2f9af3e269699c313ffb1424287c4ac40
smdsaran <saranthuppaki056@gmail.com>
2021-07-21 20:24:36 +0530

    used nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,14 @@
+import { Route, useParams } from "react-router-dom";
+
 const Welcome = () => {
+  const params = useParams();
+
   return (
     <div>
       <h1>This is Welcome Page.</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome , New User!</p>
+      </Route>
     </div>
   );
 };
